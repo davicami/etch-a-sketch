319022ad856a0980a3333d1b7505d94fe1d785d7
davicami <100150296+davicami@users.noreply.github.com>
2022-04-11 12:15:34 +0200

    Mouseover change background

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+const grid = document.getElementById('grid');
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add("grid-element")
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridElement)
+    }
+}
+
+function changeColor(e) {
+  if (e.type === 'mouseover' && !mouseDown) return
+  if (currentMode === 'rainbow') {
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+  } else if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor
+  } else if (currentMode === 'eraser') {
+    e.target.style.backgroundColor = '#fefefe'
+  }
+}
+
+/* main */
+setupGrid(DEFAULT_SIZE);
